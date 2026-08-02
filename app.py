ff94e9fe7b5044023b439bc235fd58685944b497
Tristanmdelacruz@gmail.com <your@email.com>
2026-08-02 12:18:53 -0700

    Updates Project to RC Rover with Date

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template                #importing toolboxes
 
 app = Flask(__name__)                                   #starts the website]
 
-project_one = {                                         #importing project one for the project section. importing the important info
-"name":  "Mobile Gestured Robotic Arm",                 
+project_rc_rover = {                                         #importing project one for the project section. importing the important info
+"name":  "Remote Controlled Rover",  
+"date": "September 2023 - December 2023",               
 "overview":  "As a freshman in college, I was tasked to build a robotic arm of to combine a tank a wooden chassis and 3d printed end-effector",
 "role": "Mechanical Engineering Subteam Member",
 "skills": ["Solidworks", "3D Printing"],
@@ -20,7 +21,7 @@ def about():
 
 @app.route("/projects")                                 #third page = projects section
 def projects():
-    return render_template("projects.html", project=project_one)        #render_template(location or file)
+    return render_template("projects.html", project=project_rc_rover)        #render_template(location or file)
 
 
 if __name__ == "__main__":                              #runs the websites
