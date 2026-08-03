950e17788fd2564f0594bc876afb23e0c587e101
Tristanmdelacruz@gmail.com <your@email.com>
2026-08-02 20:07:04 -0700

    adding a second project, the bell crank

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ from flask import Flask, render_template                #importing toolboxes
 
 app = Flask(__name__)                                   #starts the website]
 
-project_rc_rover = {                                         #importing project one for the project section. importing the important info
+project_LEGACYWHEELHUB = {                                         #importing project one for the project section. importing the important info
 "name":  "MARS ROVER WHEEL HUB",  
 "date": "September 2023 - December 2023",  
 "role": "Dynamics Engineer",             
 "overview":  """Designed with inspiration from Military Tanks, this Wheel Hub to be used sustain 100+ pounds of weight
 using aliumimun sheet metal and 3D printed housings and wheel""",
+
+}
+
+project_HXBELLCRACKSUSPENSION = {                                         #importing project one for the project section. importing the important info
+"name":  "Bell Crank Lateral Suspension System",  
+"date": "July 2025 - July 2026",  
+"role": "Dynamics Engineer",             
+"overview":  """Designed the Lateral Susepension System for a Hyperloop Pod. It purpose is to restrict movement of a 100+ kg pod in the 
+lateral direction. The syste is designed to experience more load in purposed to experience more oscillations as last year's suspenion system was
+overdamped.""",
 
 }
 
@@ -22,7 +32,7 @@ def about():
 
 @app.route("/projects")                                 #third page = projects section
 def projects():
-    return render_template("projects.html", project=project_rc_rover)        #render_template(location or file)
+    return render_template("projects.html", project=project_LEGACYWHEELHUB, project2=project_HXBELLCRACKSUSPENSION)        #render_template(location or file)
 
 
 if __name__ == "__main__":                              #runs the websites
