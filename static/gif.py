9051fe2afa1535599da93919f9d8caedf82b4c7e
Tristanmdelacruz@gmail.com <your@email.com>
2026-08-26 14:09:18 -0700

    testing gif

--- static/gif.py
+++ static/gif.py
@@ -1,20 +1,31 @@
-from PIL import Image
+from pathlib import Path
+
+from PIL import Image, ImageOps
+
+static_dir = Path(__file__).parent
 
 image_paths = [
-    "static/Tristan.jpg",
-    "static/Tristan_HX_HEADSHOT.jpg",
-    "static/Tristan_TTMExpo",
-    "static/TTM_Intern_Expo_Photo",
-    "static/HX_DYNAMICS_SUBTEAM",
-    "static/UCI_LEGACY_SUBTEAM",
+    static_dir / "Tristan.jpg",
+    static_dir / "HX_DYNAMICS_SUBTEAM.jpg",
+    static_dir / "UCI_LEGACY_SUBTEAM.jpg",
+    static_dir / "LeDrone_Pic.jpg",
 ]
 
-frames = [Image.open(path).convert("RGB") for path in image_paths]
+first_frame = Image.open(image_paths[0]).convert("RGB")
+frame_size = first_frame.size
+frames = []
+
+for path in image_paths:
+    image = Image.open(path).convert("RGB")
+    image = ImageOps.contain(image, frame_size)
+    frame = Image.new("RGB", frame_size, "white")
+    frame.paste(image, ((frame_size[0] - image.width) // 2, (frame_size[1] - image.height) // 2))
+    frames.append(frame)
 
 frames[0].save(
-    "static/intro.gif"
-    save_all=True
-    append_images=frames[1:]
-    duration=1500
-    loop=0
+    static_dir / "intro.gif",
+    save_all=True,
+    append_images=frames[1:],
+    duration=1500,
+    loop=0,
 )
