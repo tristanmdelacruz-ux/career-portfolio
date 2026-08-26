1025088159da9ba7cf231544c0ac83aaea91a90b
Tristanmdelacruz@gmail.com <your@email.com>
2026-08-26 14:25:06 -0700

    adding to gif

--- static/gif.py
+++ static/gif.py
@@ -7,12 +7,14 @@ static_dir = Path(__file__).parent
 image_paths = [
     static_dir / "Tristan.jpg",
     static_dir / "HX_DYNAMICS_SUBTEAM.jpg",
-    static_dir / "UCI_LEGACY_SUBTEAM.jpg",
-    static_dir / "LeDrone_Pic.jpg",
+    static_dir / "HX_11.jpg",
+    static_dir / "ASME_HEADSHOT.jpg",
+    static_dir / "asme_2026.jpg",
+    
 ]
 
 first_frame = Image.open(image_paths[0]).convert("RGB")
-frame_size = first_frame.size
+frame_size = ImageOps.contain(first_frame, (1200, 800)).size
 frames = []
 
 for path in image_paths:
